bce347f5fe01f6c99f2bdd6b37457734fe5ab5de
YokoyamaGen <g.yokoyama.0616@gmail.com>
2022-09-05 07:33:18 +0900

    Vue.jsでTrelo風アプリを作成

---
+++ src/components/TextAdd.vue
@@ -0,0 +1,36 @@
+<template>
+  <form class="addcard" @submit.prevent="addTextToList">
+    <input
+      v-model="text"
+      type="text"
+      class="text-input"
+      placeholder="Add new text"
+    />
+    <button type="submit" class="add-button">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    listIndex: {
+      type: Number,
+      required: true,
+    },
+  },
+  data: function () {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    addTextToList: function () {
+      this.$store.dispatch("addTextToList", {
+        text: this.text,
+        listIndex: this.listIndex,
+      });
+      this.text = "";
+    },
+  },
+};
+</script>
